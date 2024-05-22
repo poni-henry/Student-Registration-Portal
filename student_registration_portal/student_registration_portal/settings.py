79649b38374204ba48139da8f1fd0e3e8bc288ac
Poni Henry <ponihenry88@gmail.com>
2024-05-22 17:18:46 +0200

    made changes

--- student_registration_portal/student_registration_portal/settings.py
+++ student_registration_portal/student_registration_portal/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Replace 'staticfiles' with your desired directory name
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin/css')  # Replace 'staticfiles' with your desired directory name
 
 
 # Default primary key field type
