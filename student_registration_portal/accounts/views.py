9c9b9462d9707ccfebd9d13cf7e5a19787f39364
Poni Henry <ponihenry88@gmail.com>
2024-05-22 15:50:46 +0200

    made changes to forms and view

--- student_registration_portal/accounts/views.py
+++ student_registration_portal/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import LoginForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']  # Access authenticated user
+            login(request, user)
+            return redirect('home')  # Redirect to your desired home page after login
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', context={'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect back to login page after logout
